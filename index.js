f02e7c66dc9a55a9a4e872db1a1378c6101713a7
Angel-Banks <angelonuchuks@gmail.com>
2022-05-31 19:16:06 +0100

    updated

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+const employee = document.querySelectorAll('.employee');
+// searchEmployee.addEventListener('input', (e) =>{
+  // searchEmployee.addEventListener('input', function (e){
+
+  searchEmployee.oninput=(e)=>{
+
+    displayEmployees(e.target.value.toUpperCase());
+  };
+
+  const displayEmployees = (values) =>{
+
+// function displayEmployees (values) {
+
+    employee.forEach((staffPage)=> {
+      var name = staffPage.innerHTML.toUpperCase();
+
+        staffPage.style.display = "none"
+
+        if (name.includes(values)){
+            staffPage.style.display = "block"
+            };
+});
+};
